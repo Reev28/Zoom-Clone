cff94e6f7cad706787c0287fdc40f7046d1d68ec
Reev28 <reev.dsouza@yahoo.com>
2024-03-31 21:53:26 +0530

    mobile na

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,29 @@
+import Image from '@/node_modules/next/image'
+import Link from '@/node_modules/next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className='flex item-center gap-1'>
+        <Image
+          src="/icons/logo.svg"
+          alt="Yoom logo"
+          width={32}
+          height={32}
+          className="max-sm:size:10"
+        />
+      <p className="text-[26px] font-extrabold text-white max-sm:hidden">Xoom</p>
+
+      </Link>
+
+      <div className="flex-between gap-5">
+        {/* Clerk - User Management */}
+
+        <MobileNav/>
+      </div>
+    </nav>
   )
 }
 
